f02ee6cb4caa7b6d93de13ad00ae7221869d9a4c
vivek singh <65790028+vivek147-cyber@users.noreply.github.com>
2023-04-29 14:17:44 +0530

    first

---
+++ main_app/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
+# Register your models here.
+
+
+class UserModel(UserAdmin):
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, UserModel)
+admin.site.register(Staff)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Subject)
+admin.site.register(Session)
+admin.site.register(Tag)
+admin.site.register(Post)
